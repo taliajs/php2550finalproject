04d72f50657b954e071b806c3d5b297c19bc5430
srcuiT <95755369+srcuiT@users.noreply.github.com>
2022-10-23 17:43:25 -0400

    Update missingdata.R

--- missingdata.R
+++ missingdata.R
@@ -1,9 +1,9 @@
-library (readr)
+library(readr)
 library(naniar)
 library(ggplot2)
 library(tidyverse)
+library(kableExtra)
 library(VIM)
-library(mice)
 #Read the dataset from the Github website of raw material
 urlfile="https://raw.githubusercontent.com/taliajs/php2550finalproject/main/isolates.csv"
 isolates<-read_csv(url(urlfile))
@@ -56,8 +56,8 @@ row.plot
 #apply(isolates, 2, p_miss)
 
 # Get number of missings per variable (n and %)
-miss_var_summary(isolates)
-miss_var_table(isolates)
+miss_var_summary(isolates) %>% filter(pct_miss >50) %>% kbl(caption = "Missing Values for each Variable") %>% kable_classic()
+miss_var_table(isolates) %>% kbl(caption = "Missing Values for each Variable") %>% kable_classic()
 
 #explore if there is the missing relation between isolation source and location
 #Blue values are observed values and red ones are missing values
@@ -73,7 +73,6 @@ marginplot(isolates[,c('Location', 'Isolation type')])
 # check if a mixture of variables has missing values simultaneously
 gg_miss_upset(isolates)
 #There is a substantial number of cases in which some missings happen to occur across certain variables. 
-#We thought that the missing data type is not missing completely at random (MCAR). 
 
 #the impact of the presence of missing values in isolation type on year
 isolates_test <- isolates %>%
@@ -91,7 +90,4 @@ na_year_2020 <- isolates_test %>%
 t.test(na_year_2000, na_year_2020)
 #The percentage of missing sin isolation type has p-value smaller than 0.05.
 # We can reject null hypothesis as there is significant differnce between isolation type missingness for collecting before 2000 and after 2000.
-#Then it is expected for some reasons that the percentage of missing values in isolation type differs depending on the level of collection years.
-# In the future analysis, we may consider to separate the analysis based on levels of collection year to minimize the difference among groups that consists of isolation type variable.
-
 
