d5f437223b9805a324f6d4bcfeec354c39519f7e
srcuiT <95755369+srcuiT@users.noreply.github.com>
2022-12-14 23:31:48 -0500

    Add files via upload

--- code/Linear_Model.R
+++ code/Linear_Model.R
@@ -42,119 +42,153 @@ outbreak_df$AMR.genotypes.count <- ifelse(outbreak_df$AMR.genotypes.count>=6,1,0
 outbreak_df$SNP.cluster.rank <- ifelse(outbreak_df$SNP.cluster.rank<=21,1,2)
 
 # use those key parameters for further building predictive model on isolates numbers over time
-model_df <- outbreak_df %>% select(c("AMR.genotypes.count","Stress.genotypes.count",
+model_df_m <- outbreak_df %>% select(c("AMR.genotypes.count","Stress.genotypes.count",
                                       "isolate_source_type","Host.category","SNP.cluster.rank",
-                                      "region","Year","Month","Week","Serovar","new_outbreak")) %>%
-  group_by(Serovar,Year,Month,Week,region) %>%
+                                      "region","Year","Month","Serovar","new_outbreak")) %>%
+  group_by(Serovar,Year,Month,region) %>%
   # count number of isolate by groups, modify year variables starting 2017
   mutate(Outbreak_num=sum(new_outbreak),Year=Year-2016) %>%
   select(-c("new_outbreak"))
 
-model_df$isolate_source_type <- levels(model_df$isolate_source_type)[model_df$isolate_source_type]
+model_df_m$isolate_source_type <- levels(model_df$isolate_source_type)[model_df$isolate_source_type]
+
+model_df_w <- outbreak_df %>% select(c("AMR.genotypes.count","Stress.genotypes.count",
+                                       "isolate_source_type","Host.category","SNP.cluster.rank",
+                                       "region","Year","Week","Serovar","new_outbreak")) %>%
+  group_by(Serovar,Year,Week,region) %>%
+  # count number of isolate by groups, modify year variables starting 2017
+  mutate(Outbreak_num=sum(new_outbreak),Year=Year-2016) %>%
+  select(-c("new_outbreak"))
+
+model_df_w$isolate_source_type <- levels(model_df$isolate_source_type)[model_df$isolate_source_type]
+
 
 ########################### Gaussian Linear Regression Model ###########################
 
 ###### predicting in months ######
 
 # fit the model and checking the significance of variables
-modelm_1 <-glm(Outbreak_num~.,model_df[,-9],family="gaussian")
+modelm_1 <-glm(Outbreak_num~.,model_df_m,family="gaussian")
 #summary(modelm_1) 
 
-# need some transformation on month variable
-model_df <- model_df %>% mutate(Month=log(Month))
-modelm_2 <- glm(Outbreak_num~.,model_df[,-9],family="gaussian")
+# modify isolate source type of pork, beef into the category of other meat
+model_df_m$isolate_source_type <- factor(model_df_m$isolate_source_type)
+
+model_df_m$isolate_source_type <- relevel(model_df_m$isolate_source_type,ref="turkey")
+
+modelm_2 <- glm(Outbreak_num~.,model_df_m,family="gaussian")
 # summary(modelm_2)
 
-# relevel variables for model improving
-model_df$region <- relevel(model_df$region, ref = "unknown")
-modelm_3 <- glm(Outbreak_num~.,model_df[,-9],family="gaussian")
-# summary(modelm_3)
-
-# modify isolate source type of pork, beef into the category of other meat
-model_df$isolate_source_type[model_df$isolate_source_type=="pork"] <- "other meat"
-model_df$isolate_source_type[model_df$isolate_source_type=="beef"] <- "other meat"
-model_df$isolate_source_type <- factor(model_df$isolate_source_type)
-model_df$isolate_source_type <- relevel(model_df$isolate_source_type,ref="other meat")
-
-modelm_4 <- glm(Outbreak_num~.,model_df[,-9],family="gaussian")
-# summary(modelm_4)
-
 # modify host category of Human into mammal and reptile into other
-model_df$Host.category[model_df$Host.category=="Human"] <- "Mammal"
-model_df$Host.category[model_df$Host.category=="Reptile"] <- "Other"
-model_df$Host.category <- factor(model_df$Host.category)
-model_df$Host.category <- relevel(model_df$Host.category,ref="unknown")
+model_df_m$Host.category[model_df_m$Host.category=="Human"] <- "Mammal"
+model_df_m$Host.category[model_df_m$Host.category=="Reptile"] <- "Other"
+model_df_m$Host.category <- factor(model_df_m$Host.category)
+model_df_m$Host.category <- relevel(model_df_m$Host.category,ref="Other")
 
-modelm_5 <- glm(Outbreak_num~.,model_df[,-9],family="gaussian")
-# summary(modelm_5)
-
-# transform the year variable
-model_df <- model_df %>% mutate(Year=exp(Year))
-modelm_6 <- glm(Outbreak_num~.,model_df[,-9],family="gaussian")
-# summary(modelm_6) #final month predictive model
+modelm_3 <- glm(Outbreak_num~.,model_df_m,family="gaussian")
+# summary(modelm_3) #final predictive model in month
 
 
 ###### predicting in weeks ######
 
 # fit the model and checking the significance of variables
-modelw_1 <-glm(Outbreak_num~.,model_df[,-8],family="gaussian")
+modelw_1 <-glm(Outbreak_num~.,model_df_w,family="gaussian")
 #summary(modelw_1) 
 
 # transform week variable
-model_df <- model_df %>% mutate(Week=log(Week))
+model_df_w <- model_df_w %>% mutate(Week=log(Week))
 
-modelw_2 <-glm(Outbreak_num~.,model_df[,-8],family="gaussian")
-#summary(modelw_2) #final week predictive model
+modelw_2 <-glm(Outbreak_num~.,model_df_w,family="gaussian")
+#summary(modelw_2) 
+
+# relevel variables for model improving
+model_df_w$region <- relevel(model_df_w$region, ref = "unknown")
+modelw_3 <- glm(Outbreak_num~.,model_df_w,family="gaussian")
+# summary(modelw_3)
+
+# modify isolate source type of pork, beef into the category of other meat
+model_df_w$isolate_source_type <- factor(model_df_w$isolate_source_type)
+model_df_w$isolate_source_type <- relevel(model_df_w$isolate_source_type,ref="unknown")
+modelw_4 <- glm(Outbreak_num~.,model_df_w,family="gaussian")
+# summary(modelw_4)
+
+# modify host category of Human into mammal and reptile into other
+model_df_w$Host.category[model_df$Host.category=="Human"] <- "Mammal"
+model_df_w$Host.category[model_df$Host.category=="Reptile"] <- "Other"
+model_df_w$Host.category <- factor(model_df$Host.category)
+model_df_w$Host.category <- relevel(model_df$Host.category,ref="unknown")
+
+modelw_5 <- glm(Outbreak_num~.,model_df_w,family="gaussian")
+# summary(modelw_5)
+
+# transform the year variable
+model_df_w <- model_df_w%>% mutate(Year=exp(Year))
+modelw_6 <- glm(Outbreak_num~.,model_df_w,family="gaussian")
+# summary(modelw_6) #final predictive model in weeks
+
 
 ########################### data validation ###########################
 # cross-validation: split into train-80% and test-20% using year variable
 # dividing data into five sets, each with a training set and a test set.
-train_index <- createDataPartition(model_df$Year, p=0.8, list = FALSE, times = 5)
-train_df1 <- model_df[train_index[,1],]
-train_df2 <- model_df[train_index[,2],]
-train_df3 <- model_df[train_index[,3],]
-train_df4 <- model_df[train_index[,4],]
-train_df5 <- model_df[train_index[,5],]
+set.seed(123)
+train_index <- createDataPartition(model_df_m$Year, p=0.8, list = FALSE, times = 5)
+train_df1_m <- model_df_m[train_index[,1],]
+train_df2_m <- model_df_m[train_index[,2],]
+train_df3_m <- model_df_m[train_index[,3],]
+train_df4_m <- model_df_m[train_index[,4],]
+train_df5_m <- model_df_m[train_index[,5],]
 
-test_df1 <- model_df[-train_index[,1],]
-test_df2 <- model_df[-train_index[,2],]
-test_df3 <- model_df[-train_index[,3],]
-test_df4 <- model_df[-train_index[,4],]
-test_df5 <- model_df[-train_index[,5],]
+test_df1_m <- model_df_m[-train_index[,1],]
+test_df2_m <- model_df_m[-train_index[,2],]
+test_df3_m <- model_df_m[-train_index[,3],]
+test_df4_m <- model_df_m[-train_index[,4],]
+test_df5_m <- model_df_m[-train_index[,5],]
+
+train_df1_w <- model_df_w[train_index[,1],]
+train_df2_w <- model_df_w[train_index[,2],]
+train_df3_w <- model_df_w[train_index[,3],]
+train_df4_w <- model_df_w[train_index[,4],]
+train_df5_w <- model_df_w[train_index[,5],]
+
+test_df1_w <- model_df_w[-train_index[,1],]
+test_df2_w <- model_df_w[-train_index[,2],]
+test_df3_w <- model_df_w[-train_index[,3],]
+test_df4_w <- model_df_w[-train_index[,4],]
+test_df5_w <- model_df_w[-train_index[,5],]
 
 # fitting the model on all training data and average the coefficients
-model1_coef <- cbind(summary(glm(Outbreak_num~.,train_df1[,-9],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df2[,-9],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df3[,-9],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df4[,-9],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df5[,-9],family="gaussian"))$coefficients[,1])
+model1_coef <- cbind(summary(glm(Outbreak_num~.,train_df1_m,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df2_m,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df3_m,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df4_m,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df5_m,family="gaussian"))$coefficients[,1])
 
 avg_model1_coef <- apply(model1_coef,1,mean)
 
 
 # fitting the model on all training data and average the coefficients
-model2_coef <- cbind(summary(glm(Outbreak_num~.,train_df1[,-8],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df2[,-8],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df3[,-8],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df4[,-8],family="gaussian"))$coefficients[,1],
-                     summary(glm(Outbreak_num~.,train_df5[,-8],family="gaussian"))$coefficients[,1])
+model2_coef <- cbind(summary(glm(Outbreak_num~.,train_df1_w,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df2_w,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df3_w,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df4_w,family="gaussian"))$coefficients[,1],
+                     summary(glm(Outbreak_num~.,train_df5_w,family="gaussian"))$coefficients[,1])
 
 avg_model2_coef <- apply(model2_coef,1,mean)
 
 
 ########################### Evaluation ###########################
 
-test_df1$predict <- predict(glm(Outbreak_num~.,train_df1[,-9],family="gaussian"),test_df1,type="response")
-test_df2$predict <- predict(glm(Outbreak_num~.,train_df2[,-9],family="gaussian"),test_df2,type="response")
-test_df3$predict <- predict(glm(Outbreak_num~.,train_df3[,-9],family="gaussian"),test_df3,type="response")
-test_df4$predict <- predict(glm(Outbreak_num~.,train_df4[,-9],family="gaussian"),test_df4,type="response")
-test_df5$predict <- predict(glm(Outbreak_num~.,train_df5[,-9],family="gaussian"),test_df5,type="response")
+test_df1$predict <- predict(glm(Outbreak_num~.,train_df1_m,family="gaussian"),test_df1,type="response")
+test_df2$predict <- predict(glm(Outbreak_num~.,train_df2_m,family="gaussian"),test_df2,type="response")
+test_df3$predict <- predict(glm(Outbreak_num~.,train_df3_m,family="gaussian"),test_df3,type="response")
+test_df4$predict <- predict(glm(Outbreak_num~.,train_df4_m,family="gaussian"),test_df4,type="response")
+test_df5$predict <- predict(glm(Outbreak_num~.,train_df5_m,family="gaussian"),test_df5,type="response")
 
-test_df1$predict2 <- predict(glm(Outbreak_num~.,train_df1[,-8],family="gaussian"),test_df1,type="response")
-test_df2$predict2 <- predict(glm(Outbreak_num~.,train_df2[,-8],family="gaussian"),test_df2,type="response")
-test_df3$predict2 <- predict(glm(Outbreak_num~.,train_df3[,-8],family="gaussian"),test_df3,type="response")
-test_df4$predict2 <- predict(glm(Outbreak_num~.,train_df4[,-8],family="gaussian"),test_df4,type="response")
-test_df5$predict2 <- predict(glm(Outbreak_num~.,train_df5[,-8],family="gaussian"),test_df5,type="response")
+test_df1$predict2 <- predict(glm(Outbreak_num~.,train_df1_w,family="gaussian"),test_df1,type="response")
+test_df2$predict2 <- predict(glm(Outbreak_num~.,train_df2_w,family="gaussian"),test_df2,type="response")
+test_df3$predict2 <- predict(glm(Outbreak_num~.,train_df3_w,family="gaussian"),test_df3,type="response")
+test_df4$predict2 <- predict(glm(Outbreak_num~.,train_df4_w,family="gaussian"),test_df4,type="response")
+test_df5$predict2 <- predict(glm(Outbreak_num~.,train_df5_w,family="gaussian"),test_df5,type="response")
 
 roc.model1_test1 <- roc(test_df1$Outbreak_num,test_df1$predict)
 roc.model1_test2 <- roc(test_df2$Outbreak_num,test_df2$predict)
@@ -179,23 +213,23 @@ AUC_df <- data.frame(test1=c(auc(roc.model1_test1),auc(roc.model2_test1)),
                      test4=c(auc(roc.model1_test4),auc(roc.model2_test1)),
                      test5=c(auc(roc.model1_test5),auc(roc.model2_test1)))
 
-r_squared_df <- data.frame(test1=c(summary(lm(Outbreak_num~.,train_df1[,-8]))$adj.r.squared,summary(lm(Outbreak_num~.,train_df1[,-7]))$adj.r.squared),
-test2=c(summary(lm(Outbreak_num~.,train_df2[,-8]))$adj.r.squared,summary(lm(Outbreak_num~.,train_df2[,-7]))$adj.r.squared),
-test3=c(summary(lm(Outbreak_num~.,train_df3[,-8]))$adj.r.squared,summary(lm(Outbreak_num~.,train_df3[,-7]))$adj.r.squared),
-test4=c(summary(lm(Outbreak_num~.,train_df4[,-8]))$adj.r.squared,summary(lm(Outbreak_num~.,train_df4[,-7]))$adj.r.squared),
-test5=c(summary(lm(Outbreak_num~.,train_df5[,-8]))$adj.r.squared,summary(lm(Outbreak_num~.,train_df5[,-7]))$adj.r.squared))
+r_squared_df <- data.frame(test1=c(summary(lm(Outbreak_num~.,train_df1_m))$adj.r.squared,summary(lm(Outbreak_num~.,train_df1_w))$adj.r.squared),
+test2=c(summary(lm(Outbreak_num~.,train_df2_m))$adj.r.squared,summary(lm(Outbreak_num~.,train_df2_w))$adj.r.squared),
+test3=c(summary(lm(Outbreak_num~.,train_df3_m))$adj.r.squared,summary(lm(Outbreak_num~.,train_df3_w))$adj.r.squared),
+test4=c(summary(lm(Outbreak_num~.,train_df4_m))$adj.r.squared,summary(lm(Outbreak_num~.,train_df4_w))$adj.r.squared),
+test5=c(summary(lm(Outbreak_num~.,train_df5_m))$adj.r.squared,summary(lm(Outbreak_num~.,train_df5_w))$adj.r.squared))
 
-AIC_df <- data.frame(test1=c(AIC(lm(Outbreak_num~.,train_df1[,-8])),AIC(lm(Outbreak_num~.,train_df1[,-7]))),
-                     test2=c(AIC(lm(Outbreak_num~.,train_df2[,-8])),AIC(lm(Outbreak_num~.,train_df2[,-7]))),
-                     test3=c(AIC(lm(Outbreak_num~.,train_df3[,-8])),AIC(lm(Outbreak_num~.,train_df3[,-7]))),
-                     test4=c(AIC(lm(Outbreak_num~.,train_df4[,-8])),AIC(lm(Outbreak_num~.,train_df4[,-7]))),
-                     test5=c(AIC(lm(Outbreak_num~.,train_df5[,-8])),AIC(lm(Outbreak_num~.,train_df5[,-7]))))
+AIC_df <- data.frame(test1=c(AIC(lm(Outbreak_num~.,train_df1_m)),AIC(lm(Outbreak_num~.,train_df1_w))),
+                     test2=c(AIC(lm(Outbreak_num~.,train_df2_m)),AIC(lm(Outbreak_num~.,train_df2_w))),
+                     test3=c(AIC(lm(Outbreak_num~.,train_df3_m)),AIC(lm(Outbreak_num~.,train_df3_w))),
+                     test4=c(AIC(lm(Outbreak_num~.,train_df4_m)),AIC(lm(Outbreak_num~.,train_df4_w))),
+                     test5=c(AIC(lm(Outbreak_num~.,train_df5_m)),AIC(lm(Outbreak_num~.,train_df5_w))))
 
-BIC_df <- data.frame(test1=c(BIC(lm(Outbreak_num~.,train_df1[,-8])),BIC(lm(Outbreak_num~.,train_df1[,-7]))),
-                     test2=c(BIC(lm(Outbreak_num~.,train_df2[,-8])),BIC(lm(Outbreak_num~.,train_df2[,-7]))),
-                     test3=c(BIC(lm(Outbreak_num~.,train_df3[,-8])),BIC(lm(Outbreak_num~.,train_df3[,-7]))),
-                     test4=c(BIC(lm(Outbreak_num~.,train_df4[,-8])),BIC(lm(Outbreak_num~.,train_df4[,-7]))),
-                     test5=c(BIC(lm(Outbreak_num~.,train_df5[,-8])),BIC(lm(Outbreak_num~.,train_df5[,-7]))))
+BIC_df <- data.frame(test1=c(BIC(lm(Outbreak_num~.,train_df1_m)),BIC(lm(Outbreak_num~.,train_df1_w))),
+                     test2=c(BIC(lm(Outbreak_num~.,train_df2_m)),BIC(lm(Outbreak_num~.,train_df2_w))),
+                     test3=c(BIC(lm(Outbreak_num~.,train_df3_m)),BIC(lm(Outbreak_num~.,train_df3_w))),
+                     test4=c(BIC(lm(Outbreak_num~.,train_df4_m)),BIC(lm(Outbreak_num~.,train_df4_w))),
+                     test5=c(BIC(lm(Outbreak_num~.,train_df5_m)),BIC(lm(Outbreak_num~.,train_df5_w))))
 
 Comparison_df <- data.frame(AUC=round(apply(AUC_df,1,mean),2),
                             R_squared=round(apply(r_squared_df,1,mean),2),
@@ -208,16 +242,16 @@ colnames(eval) <- c("Model of Months","Model of Weeks")
 
 # evaluation of Gaussian linear regression model
 eval %>%
-  kbl() %>%
-  kable_paper("hover", full_width = F)
+  kbl(caption="Comparison Table of Measuring Metrics of 2 Models") %>%
+  kable_classic(full_width = F, html_font = "Cambria")
 
 
 
 ########################### Example ###########################
 # observed data for a specific group: Typhimurium Serovar in egg, chicken, turkey and poultry
-x_vars_month <- model.matrix(Outbreak_num~.,model_df[,-9])
+x_vars_month <- model.matrix(Outbreak_num~.,model_df_m)
 predict_month <- x_vars_month %*% avg_model1_coef
-x_vars_week <- model.matrix(Outbreak_num~.,model_df[,-8])
+x_vars_week <- model.matrix(Outbreak_num~.,model_df_w)
 predict_week <- x_vars_week %*% avg_model2_coef
 example <- cbind(model_df,predict_month=predict_month,predict_week=predict_week) %>% 
   filter(Serovar=="Typhimurium",isolate_source_type %in% c("egg","chicken","turkey","poultry")) %>%
